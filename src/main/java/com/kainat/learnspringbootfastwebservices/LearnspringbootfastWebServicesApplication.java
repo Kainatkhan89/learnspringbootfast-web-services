970c415247de300425aa5723fef61694dc6b7370
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-26 19:29:44 -0400

    initial commit

---
+++ src/main/java/com/kainat/learnspringbootfastwebservices/LearnspringbootfastWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.kainat.learnspringbootfastwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspringbootfastWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnspringbootfastWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kainat/learnspringbootfastwebservices/LearnspringbootfastWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kainat.learnspringbootfastwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspringbootfastWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
