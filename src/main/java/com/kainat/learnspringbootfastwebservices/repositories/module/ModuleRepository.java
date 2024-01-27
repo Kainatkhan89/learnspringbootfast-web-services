8b0d9ba51559a3c55ed4bc6279bf76dd88ebc661
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-27 09:39:01 -0400

    remove unused import

--- src/main/java/com/kainat/learnspringbootfastwebservices/repositories/module/ModuleRepository.java
+++ src/main/java/com/kainat/learnspringbootfastwebservices/repositories/module/ModuleRepository.java
@@ -4,7 +4,6 @@ import com.kainat.learnspringbootfastwebservices.entities.module.Module;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface ModuleRepository extends JpaRepository<Module, Integer> {
