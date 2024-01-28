d59811f4f814404bac81a9040b66bb2049928e24
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-27 22:48:43 -0400

    Define CompletedTutorialDTO

---
+++ src/main/java/com/kainat/learnspringbootfastwebservices/dtos/progress/CompletedTutorialDTO.java
@@ -0,0 +1,16 @@
+package com.kainat.learnspringbootfastwebservices.dtos.progress;
+
+public class CompletedTutorialDTO {
+    private int tutorialId;
+
+    public CompletedTutorialDTO() {
+    }
+
+    public int getTutorialId() {
+        return this.tutorialId;
+    }
+
+    public void setTutorialId(int tutorialId) {
+        this.tutorialId = tutorialId;
+    }
+}
