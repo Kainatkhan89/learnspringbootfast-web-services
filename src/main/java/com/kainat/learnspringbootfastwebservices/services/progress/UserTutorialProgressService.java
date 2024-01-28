73c78352b42558a756360e4effa643f1959c7a65
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-27 22:11:58 -0400

    Create UserTutorialProgressService interface

--- src/main/java/com/kainat/learnspringbootfastwebservices/services/progress/UserTutorialProgressService.java
+++ src/main/java/com/kainat/learnspringbootfastwebservices/services/progress/UserTutorialProgressService.java
@@ -1,7 +1,8 @@
 package com.kainat.learnspringbootfastwebservices.services.progress;
 
+import com.kainat.learnspringbootfastwebservices.dtos.progress.ProgressUpdateRequest;
 import com.kainat.learnspringbootfastwebservices.entities.progress.UserTutorialProgress;
 
 public interface UserTutorialProgressService {
-    UserTutorialProgress markTutorialAsCompleted();
+    UserTutorialProgress markTutorialAsCompleted(ProgressUpdateRequest progressUpdateRequest);
 }
