c1ae6302fda757fd579ce45928f76b763e0b0d7c
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-29 19:36:15 -0400

    remove unused import

--- src/main/java/com/kainat/learnspringbootfastwebservices/controllers/progress/UserTutorialProgressController.java
+++ src/main/java/com/kainat/learnspringbootfastwebservices/controllers/progress/UserTutorialProgressController.java
@@ -5,7 +5,6 @@ import com.kainat.learnspringbootfastwebservices.dtos.progress.ProgressUpdateReq
 import com.kainat.learnspringbootfastwebservices.entities.progress.UserTutorialProgress;
 import com.kainat.learnspringbootfastwebservices.services.progress.UserTutorialProgressService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
