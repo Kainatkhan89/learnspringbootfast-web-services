b6658da97231fdfed2e82186f56ab278bcd86a28
kainatkhan89 <kainatkhan892014@gmail.com>
2024-02-04 19:17:41 -0400

    add findByUserId() repo method

--- src/main/java/com/kainat/learnspringbootfastwebservices/repositories/LearningProgressRepository.java
+++ src/main/java/com/kainat/learnspringbootfastwebservices/repositories/LearningProgressRepository.java
@@ -3,5 +3,8 @@ package com.kainat.learnspringbootfastwebservices.repositories;
 import com.kainat.learnspringbootfastwebservices.entities.CompletedTutorial;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LearningProgressRepository extends JpaRepository<CompletedTutorial, Integer> {
+    public List<CompletedTutorial> findByUserId(String userId);
 }
