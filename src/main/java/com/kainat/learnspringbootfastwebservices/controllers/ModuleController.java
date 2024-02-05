07edfc1b7e42eb53effad36a5124fdef72419e4d
kainatkhan89 <kainatkhan892014@gmail.com>
2024-02-05 08:03:09 -0400

    implement ModuleService

--- src/main/java/com/kainat/learnspringbootfastwebservices/controllers/ModuleController.java
+++ src/main/java/com/kainat/learnspringbootfastwebservices/controllers/ModuleController.java
@@ -2,6 +2,7 @@ package com.kainat.learnspringbootfastwebservices.controllers;
 
 import com.kainat.learnspringbootfastwebservices.entities.Module;
 import com.kainat.learnspringbootfastwebservices.repositories.ModuleRepository;
+import com.kainat.learnspringbootfastwebservices.services.ModuleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,16 +14,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/modules")
 public class ModuleController {
-    private final ModuleRepository moduleRepository;
+    private final ModuleService moduleService;
 
     @Autowired
-    public ModuleController(ModuleRepository moduleRepository) {
-        this.moduleRepository = moduleRepository;
+    public ModuleController(ModuleService moduleService) {
+        this.moduleService = moduleService;
     }
 
     @GetMapping
     public ResponseEntity<List<Module>> getModules() {
-        List<Module> modules = this.moduleRepository.getAllWithTutorials();
+        List<Module> modules = this.moduleService.getAllModulesWithTheirTutorials();
 
         return ResponseEntity.ok(modules);
     }
